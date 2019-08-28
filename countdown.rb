0b9939d767815ccce389cdce33e13f9e418c4da0
wei chen <weichen9083@gmail.com>
2019-08-28 21:55:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,13 @@
 def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
-  sleep(1)
   number -= 1
+  countdown_with_sleep(1)
 end 
 "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(integer)
+  sleep(integer)
+end 
